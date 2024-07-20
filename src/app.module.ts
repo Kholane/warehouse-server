a616391cc5054be3865282ef5d0ea8c57aecc174
ColeColombia <benzemabenelzane@yahoo.com>
2024-07-20 18:33:42 +0200

    remove product controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductControllerController } from './product-controller/product-controller.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductControllerController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/product-controller/product-controller.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('product-controller')
-export class ProductControllerController {}
